fe28df0e0036eca0c82f9bb9ac1f4d52d158336e
juniarto <juniarto.samsudin@gmail.com>
2020-09-11 16:14:09 +0800

    edit waitress

--- waitress_server.py
+++ waitress_server.py
@@ -5,6 +5,7 @@ from line.lineApp import lineApp
 from histogram.histoApp import histoApp
 from base.baseApp import baseApp
 from spc.spcApp import spcApp
+from restdatagenerator.restdatageneratorApp import restdatageneratorApp
 from waitress import serve
 
 app = Flask(__name__)
@@ -14,6 +15,7 @@ app.register_blueprint(lineApp, url_prefix="/line")
 app.register_blueprint(histoApp, url_prefix="/histogram")
 app.register_blueprint(baseApp, url_prefix="/base")
 app.register_blueprint(spcApp, url_prefix="/spc")
+app.register_blueprint(restdatageneratorApp,url_prefix="/restdatagenerator")
 
 @app.route('/')
 def default():
